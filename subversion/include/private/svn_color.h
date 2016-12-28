cdf8b0900b09e47303867d5b208b246283e6f563
wuzhouhui <wuzhouhui250@gmail.com>
2016-12-28 22:33:55 +0800

    Pring log header with color magenta

--- subversion/include/private/svn_color.h
+++ subversion/include/private/svn_color.h
@@ -6,5 +6,7 @@
 #define SVN_COLOR_GREEN        "\033[32m"
 #define SVN_COLOR_BLUE         "\033[34m"
 #define SVN_COLOR_YELLOW       "\033[33m"
+#define SVN_COLOR_MAGENTA      "\033[35m"
+#define SVN_COLOR_CYAN         "\033[36m"
 
 #endif /* SVN_COLOR_H */
