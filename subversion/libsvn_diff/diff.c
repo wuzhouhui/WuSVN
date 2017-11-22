af2689af1ef648bbb23fd0d4d54f1a9cd9397f62
wuzhouhui <wuzhouhui250@gmail.com>
2017-11-22 21:21:24 +0800

    Scale max width for diffstat, for better look

--- subversion/libsvn_diff/diff.c
+++ subversion/libsvn_diff/diff.c
@@ -972,6 +972,7 @@ svn_diff_create_dfctx(svn_dfstat_ctx_t **ctx)
         }
       max_width = winsz.ws_col;
     }
+  max_width--;
 
   merge_names = 1;	/* true if we merge similar filenames */
   merge_opt = 0;	/* true if we merge ins/del as modified */
